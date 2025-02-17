ab1275e0e2de058ff0aead4d2ffe7afa29063a4b
LucasGatica <lucas.gatica79@gmail.com>
2025-02-17 17:33:46 -0300

    aula 2 - implementando segundo node
    segundo nó

--- src/codeAcademy/node/Node.java
+++ src/codeAcademy/node/Node.java
@@ -10,10 +10,18 @@ public class Node {
         this.next = null;
     }
 
+    public void setNextNode(Node node){
+        this.next = node;
+    }
+
     public static void main(String[] args) {
         Node firstNode = new Node("I am a Node!");
         System.out.println(firstNode.data);
-        System.out.println(firstNode.next);
 
+        Node secondNode = new Node("I am the second Node!");
+
+        firstNode.setNextNode(secondNode);
+
+        System.out.println(firstNode.next.data);
     }
 }
