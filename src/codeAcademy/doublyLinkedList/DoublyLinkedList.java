324d210df5ec93f3544923eba3d249ed046668f5
LucasGatica <lucas.gatica79@gmail.com>
2025-02-18 14:23:13 -0300

    aula1 doublelinkedList - novo tipo de node

---
+++ src/codeAcademy/doublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,11 @@
+package codeAcademy.doublyLinkedList;
+
+public class DoublyLinkedList {
+    public Node head;
+    public Node tail;
+
+    public DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+    }
+}
