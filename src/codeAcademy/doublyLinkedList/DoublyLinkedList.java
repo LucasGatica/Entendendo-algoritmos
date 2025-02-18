bd866d42865b19e061decb3e2e1dc78c10f87b50
LucasGatica <lucas.gatica79@gmail.com>
2025-02-18 14:31:38 -0300

    aula2 doubleLinkedList - addTohead

--- src/codeAcademy/doublyLinkedList/DoublyLinkedList.java
+++ src/codeAcademy/doublyLinkedList/DoublyLinkedList.java
@@ -8,4 +8,30 @@ public class DoublyLinkedList {
         this.head = null;
         this.tail = null;
     }
+
+    public void addToHead(String data){
+        Node newHead = new Node(data);
+        Node currentHead = this.head;
+
+        if(currentHead!=null){
+            currentHead.setPreviousNode(newHead);
+            newHead.setNextNode(currentHead);
+        }
+        this.head = newHead;
+
+        if(this.tail==null){
+            this.tail= newHead;
+        }
+    }
+    public String printList() {
+        Node currentNode = this.head;
+        String output = "<head> ";
+        while (currentNode != null) {
+            output += currentNode.data + " ";
+            currentNode = currentNode.getNextNode();
+        }
+        output += "<tail>";
+        System.out.println(output);
+        return output;
+    }
 }
