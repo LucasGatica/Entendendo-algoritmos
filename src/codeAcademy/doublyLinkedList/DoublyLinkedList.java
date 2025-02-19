60bbbe2c45ed3fb012d775be672819ebe641de2c
LucasGatica <lucas.gatica79@gmail.com>
2025-02-19 07:21:55 -0300

    aula 6 - testando a double linkedlist

--- src/codeAcademy/doublyLinkedList/DoublyLinkedList.java
+++ src/codeAcademy/doublyLinkedList/DoublyLinkedList.java
@@ -103,4 +103,24 @@ public class DoublyLinkedList {
         System.out.println(output);
         return output;
     }
+
+    public static void main(String[] args) {
+        DoublyLinkedList subway = new DoublyLinkedList();
+        subway.addToHead("Times Square");
+        subway.addToHead("Grand Central");
+        subway.addToHead("Central Park");
+        subway.printList();
+        subway.addToTail("Penn Station");
+        subway.addToTail("Wall Street");
+        subway.addToTail("Brooklyn Bridge");
+        subway.printList();
+
+        subway.removeHead();
+        subway.removeTail();
+        subway.printList();
+
+        subway.removeByData("Times Square");
+        subway.printList();
+
+    }
 }
