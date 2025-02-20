a5d9ae561e316bd251279364a67315889bdc4be5
LucasGatica <lucas.gatica79@gmail.com>
2025-02-20 09:22:09 -0300

    linkedilists - funcao para mostrar o node anterior de um valor

---
+++ src/codeAcademy/linkedlist/NLastNode.java
@@ -0,0 +1,40 @@
+package codeAcademy.linkedlist;
+
+
+import codeAcademy.node.Node;
+
+public class NLastNode {
+
+    public static void main(String []args) {
+        // Use this to test your code:
+        LinkedList ll = createList();
+        ll.printList();
+        Node n = nthLastNode(ll, 50);
+        System.out.print(n.data);
+    }
+
+    public static Node nthLastNode(LinkedList list, int n) {
+        Node nthLastNode = null;
+        Node currentNode = list.head;
+        while(currentNode!=null){
+            if(currentNode.getNextNode()==null){
+                currentNode = currentNode.getNextNode();
+                break;
+            }
+            if (currentNode.getNextNode().data.equals(String.valueOf(n))){
+                nthLastNode = currentNode;
+            }
+            currentNode= currentNode.getNextNode();
+        }
+        return nthLastNode;
+    }
+
+    public static LinkedList createList() {
+        LinkedList list = new LinkedList();
+        for (int i = 50; i >= 1; i--) {
+            list.addToTail(String.valueOf(i));
+        }
+        return list;
+    }
+
+}
