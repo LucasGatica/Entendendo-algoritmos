2e3e8016053b8d7cd135cd20fe5de3d26a8766c5
LucasGatica <lucas.gatica79@gmail.com>
2025-02-20 17:26:45 -0300

    aula 1 - algoritmos de busca
    busca binaria

---
+++ src/codeAcademy/binarySearch/BinarySearch.java
@@ -0,0 +1,28 @@
+package codeAcademy.binarySearch;
+
+public class BinarySearch {
+    public static int search(int[] arr, int target) {
+        int left =0;
+        int right = arr.length;
+        while(right>left) {
+        int mid = Math.floorDiv((left+right), 2);
+        int midValue = arr[mid];
+            if (midValue == target) {
+                return mid;
+            }
+            if(target>midValue){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] searchable = {1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94,100};
+        int target = 100;
+        System.out.println(search(searchable, target));
+    }
+
+}
