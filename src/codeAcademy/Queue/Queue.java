69c0fd9e51ec1382b39c22926c9e6387620438ea
LucasGatica <lucas.gatica79@gmail.com>
2025-02-24 08:30:24 -0300

    aula5 queue - logica de underflow

--- src/codeAcademy/Queue/Queue.java
+++ src/codeAcademy/Queue/Queue.java
@@ -31,26 +31,34 @@ public class Queue {
         this.size++;
         System.out.println("Added"+data+"! Queue size is now "+ this.size);
     }
-    public String dequeue(){
-        String data = this.queue.removeHead();
-        this.size--;
-        System.out.println("Removed "+data+"! Queue size is now "+this.size);
-        return data;
+    public String dequeue() {
+        if(!isEmpty()){
+            String data = this.queue.removeHead();
+            this.size--;
+            System.out.println("Removed "+data+"! Queue size is now "+this.size);
+            return data;
+        }
+        throw new Error("Queue is empty!");
     }
 
     public String peek(){
-        return this.queue.head.data;
+        if(!isEmpty()){
+            return this.queue.head.data;
+        }
+        return null;
     }
 
     public static void main(String[] args) {
-      /*
-      Queue queueOne = new Queue(25);
-      Queue queueTwo = new Queue(0);
-      System.out.println("queueOne has space for more nodes: " + queueOne.hasSpace());
-      System.out.println("queueTwo has space for more nodes: " + queueTwo.hasSpace());
-      System.out.println("queueOne is empty: " + queueOne.isEmpty());
-      System.out.println("queueTwo is empty: " + queueTwo.isEmpty());
-      */
+
+      Queue boundedQueue = new Queue(3);
+      boundedQueue.enqueue("one");
+      boundedQueue.enqueue("two");
+      boundedQueue.enqueue("three");
+
+      boundedQueue.dequeue();
+      boundedQueue.dequeue();
+      boundedQueue.dequeue();
+
 
     }
 }
